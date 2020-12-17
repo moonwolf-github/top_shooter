46a6e5ac7c8ff854813cdb839b0334e9066a5674
Marcin Trendota <marcin@trendota.com>
2020-12-17 18:21:15 +0100

    Random enemy start position, ability to restart game

--- scenes/World.gd
+++ scenes/World.gd
@@ -1,14 +1,35 @@
 extends Node2D
 
+onready var Enemy = load("scenes/Enemy.tscn")
+onready var screen_size = get_viewport_rect().size
+
+func spawn_enemy():
+    var enemy = Enemy.instance()
+    var pos = Vector2()
+    while (pos - $Player.position).length() < 2:
+        print((pos - $Player.position).length())
+        pos.x = randf() * screen_size.x
+        pos.y = randf() * screen_size.y
+    enemy.position = pos
+    add_child(enemy)
+    
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var Enemy = load("scenes/Enemy.tscn")
-    var enemy = Enemy.instance()
-    add_child(enemy)
-
+    spawn_enemy()
 
 func end_game(player_win):
+    $Button.visible = true
     if player_win:
         $Label.text = "WYGRANA!"
     $Label.visible = true
     get_tree().paused = true
+
+func _on_Button_pressed():
+    $Button.visible = false
+    $Player.transform = Transform2D()
+    $Label.visible = false
+    for child in get_children():
+        if child.is_in_group("bullets") or child.is_in_group("enemy"):
+            child.queue_free()
+    spawn_enemy()
+    get_tree().paused = false
